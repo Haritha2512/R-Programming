3e087c989e2f12ca58fbf26223d6a2ea2c541163
Haritha2512 <harithask12@gmail.com>
2026-07-24 09:41:32 +0530

    Add files via upload

---
+++ r lab - 1.R
@@ -0,0 +1,23 @@
+num_vector <- c(10, 20, 30, 40, 50)
+
+char_vector <- c("Apple", "Banana", "Mango")
+
+log_vector <- c(TRUE, FALSE, TRUE, FALSE)
+
+print("Numeric Vector:")
+print(num_vector)
+
+print("Character Vector:")
+print(char_vector)
+
+print("Logical Vector:")
+print(log_vector)
+
+print("Data Type of Numeric Vector:")
+print(typeof(num_vector))
+
+print("Data Type of Character Vector:")
+print(typeof(char_vector))
+
+print("Data Type of Logical Vector:")
+print(typeof(log_vector))
